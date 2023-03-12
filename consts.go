ede042cdc49d5befd027553274bc3dcd8d99815d
Rahul-P <rahul@pawar.com.au>
2023-03-12 22:59:55 +1100

    Time Updated.

--- consts.go
+++ consts.go
@@ -3,8 +3,12 @@ package roaringcompany
 const ZERO = 0
 const ONE = 1
 const TWO = 2
+const NINE = 9
 const TEN = 10
 const FIVEHUNDRED = 500
 
 const TRUE bool = true
 const FALSE bool = false
+
+const Digit_ZERO string = "0"
+const Digit_NINE string = "9"
--- time.go
+++ time.go
@@ -7,6 +7,11 @@ import (
 
 const DatabaseTimeout = time.Second * 5
 
+func GetTimeStamp() (mmdd uint16, year uint8, timeofDayInSeconds int) {
+	nowis := time.Now()
+	return uint16(MMDD_From_Month_Date(int(nowis.Month()), int(nowis.Day()))), uint8(YY_From_YYYY(nowis.Year())), TimeInSeconds(nowis)
+}
+
 func CurrentTimeStampV2() (day uint8, month uint8, year uint8, timeofDayInSeconds int) {
 	nowis := time.Now()
 	return uint8(nowis.Day()), uint8(int(nowis.Month())), uint8(YY_From_YYYY(nowis.Year())), TimeInSeconds(nowis)
@@ -25,6 +30,29 @@ func YY_From_YYYY(passedYYYY int) int {
 	stringTargetYear := strconv.Itoa(passedYYYY)
 	yy := stringTargetYear[2:]
 	returnYY, _ := strconv.Atoi(yy)
+
+	return returnYY
+}
+
+func MMDD_From_Month_Date(month, date int) int {
+
+	stringTargetMonth := Digit_NINE
+	monthStr := strconv.Itoa(month)
+	if month < TEN {
+		stringTargetMonth += monthStr
+	} else {
+		stringTargetMonth = monthStr
+	}
+
+	stringTargetDate := Digit_ZERO
+	dateStr := strconv.Itoa(date)
+	if date < TEN {
+		stringTargetDate += dateStr
+	} else {
+		stringTargetDate = dateStr
+	}
+
+	returnYY, _ := strconv.Atoi(stringTargetMonth + stringTargetDate)
 
 	return returnYY
 }
